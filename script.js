3048b5f21858fba8127a5a75e11035f01b69af7b
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-03-13 14:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //your JS code here. If required.
 
 function startRendering(){
-	const text =  "We love Programming!"
+	const text =  "e love Programming!"
 	const inputSpeed= document.getElementById("speed").value;
 	const output = document.getElementById("text");
 
 	let delay = 500/ parseInt(inputSpeed);
 	
-	output.textContent="";
+	output.textContent="W";
 	
 	let index = 0;
 
