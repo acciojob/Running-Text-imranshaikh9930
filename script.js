3d4345a5301cf3fcf3f0565d761e5ef0e0df26cd
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-03-13 14:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+function startRendering(){
+	const text =  "We love Programming!"
+	const inputSpeed= document.getElementById("speed").value;
+	const output = document.getElementById("text");
+
+	let delay = 500/ parseInt(inputSpeed);
+	
+	output.textContent="";
+	
+	let index = 0;
+
+	function renderText(){
+
+		if(index < text.length){
+			output.textContent+= text[index];
+			index++;
+
+			setTimeout(renderText,delay)
+		}
+	}
+
+	renderText();
+	
+}
